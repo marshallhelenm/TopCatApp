565b70cacda260d1d29f0fe3d7beb3a22de6ca63
marshallhelenm <marshallhelenm@gmail.com>
2019-08-19 17:47:45 -0700

    fixed routing error

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
     has_many :cats
 
     has_secure_password
+    
+    validates :username, presence: true
+    validates :username, uniqueness: true
+    validates :password, presence: true
+    validates :password, confirmation: { case_sensitive: true }
 end
