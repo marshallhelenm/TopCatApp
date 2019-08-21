cb66f975f818626090e114f935159652a7d00f21
marshallhelenm <marshallhelenm@gmail.com>
2019-08-21 11:06:53 -0700

    created seeds for families

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,40 +22,56 @@ c2.set_stats
 
 
 
+n1 = Neighborhood.create(name: "Graystone Heights", description: "increasingly swank", danger_rating: 2)
 
-f1 = Family.create(name: "Johnson", description: "IDK man, they're average.", poshness: "2")
-f2 = Family.create(name: "Franklin", description: "The snootiest family, with the highest grade wet food.", poshness: "3")
-f3 = Family.create(name: "Smith", description: "They aren't fancy, but they sure do love cats.", poshness: "1")
-f4 = Family.create(name: "Schubert", description: "They've got decent stuff.", poshness: "2")
-f5 = Family.create(name: "Moonstar", description: "Hippies have the best food.", poshness: "2")
-f6 = Family.create(name: "Neville", description: "Hippies have the best food.", poshness: "2")
-f7 = Family.create(name: "Wilcox", description: "Hippies have the best food.", poshness: "2")
-f8 = Family.create(name: "Hodson", description: "Hippies have the best food.", poshness: "2")
-f9 = Family.create(name: "Moonstar", description: "Hippies have the best food.", poshness: "2")
-f10 = Family.create(name: "Moonstar", description: "Hippies have the best food.", poshness: "2")
-f11 = Family.create(name: "Moonstar", description: "Hippies have the best food.", poshness: "2")
-f12 = Family.create(name: "Moonstar", description: "Hippies have the best food.", poshness: "2")
-f13 = Family.create(name: "Moonstar", description: "Hippies have the best food.", poshness: "2")
-f14 = Family.create(name: "Moonstar", description: "Hippies have the best food.", poshness: "2")
-f15 = Family.create(name: "Moonstar", description: "Hippies have the best food.", poshness: "2")
-f16 = Family.create(name: "Moonstar", description: "Hippies have the best food.", poshness: "2")
+f1 = Family.create(name: "Johnson", description: "IDK man, they're average.", poshness: 2)
+f2 = Family.create(name: "Franklin", description: "The snootiest family, with the highest grade wet food.", poshness: 3)
+f3 = Family.create(name: "Smith", description: "They aren't fancy, but they sure do love cats.", poshness: 1)
+f4 = Family.create(name: "Schubert", description: "They've got decent stuff.", poshness: 2)
+
+n1.families << f1 << f2 << f3 << f4
+
+
+n2 = Neighborhood.create(name: "Brighthope", description: "Hippies love cats.", danger_rating: 1)
+
+f5 = Family.create(name: "Moonstar", description: "They're super into ethical, natural pet food. It's pretty good.", poshness: 2)
+f6 = Family.create(name: "Albert", description: "They're vegan, and don't seem to get that cats are obligate carnivores. Still, they have some treats for you.", poshness: 1)
+f7 = Family.create(name: "Neville", description: "These hippies love cats.", poshness: 2)
+f8 = Family.create(name: "Poletti", description: "These hippies love cats.", poshness: 2)
+
+n2.families << f5 << f6 << f7 << f8
+
+
+
+n3 = Neighborhood.create(name: "Uptown", description: "So Posh", danger_rating: 1)
+
+f9 = Family.create(name: "Cunningham", description: "The snootiest family, with the highest grade wet food.", poshness: 3)
+f10 = Family.create(name: "Martinez", description: "The snootiest family, with the highest grade wet food.", poshness: 3)
+f11 = Family.create(name: "Wilcox", description: "The snootiest family, with the highest grade wet food.", poshness: 3)
+f12 = Family.create(name: "McGee", description: "They're not nearly snooty enough for this neighborhood - but maybe that's a good thing.", poshness: 2)
+
+n3.families << f9 << f10 << f11 << f12
+
+
+
+n4 = Neighborhood.create(name: "Wrongside", description: "Dirty alleyways abound.", danger_rating: 3)
+
+f13 = Family.create(name: "Hodson", description: "All they have to offer is dinner scraps, but they're provided with love.", poshness: 1)
+f14 = Family.create(name: "Coleman", description: "The cops are watching this place, but hey, you're a cat. None of your business.", poshness: 1)
+f15 = Family.create(name: "Spence", description: "Mr. Spence is a beloved teacher at the local high school.", poshness: 1)
+f16 = Family.create(name: "Whitakker", description: "A pretty average family with 2.5 kids and a very busted white picket fence.", poshness: 2)
+
+n4.families << f13 << f14 << f15 << f16
+
+
+
+
 
 f1.cats << c1
 
-n1 = Neighborhood.create(name: "Graystone Heights", description: "increasingly swank", danger_rating: 2)
-n2 = Neighborhood.create(name: "Brighthope", description: "Hippies love cats.", danger_rating: 1)
-n3 = Neighborhood.create(name: "Uptown", description: "So Posh", danger_rating: 1)
-n4 = Neighborhood.create(name: "Wrongside", description: "Dirty alleyways abound.", danger_rating: 3)
 
 
 c1.neighborhoods << n1 << n2
-
-n1.families << f1 << f2
-n2.families << f5
-n3.families << f4
-n4.families << f3
-
-
 
 
 
